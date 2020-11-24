f3d2e4f0a0756cb976b068697d7e32d48863f26f
Victor Sampaio <victor.msampaio@outlook.com.br>
2020-11-23 21:02:44 -0300

    Realizado exercícios 1019, 1020, 1021

---
+++ uri_1019/uri_1019/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uri_1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int segundos;
+
+            segundos = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
---
+++ uri_1020/uri_1020/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uri_1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input, resto, qtdAnos, qtdMeses, qtdDias, ano = 365, mes = 30;
+
+            input = int.Parse(Console.ReadLine());
+
+            qtdAnos = input / ano;
+            resto = input % ano;
+            Console.WriteLine(qtdAnos + " ano(s)");
+
+            qtdMeses = resto / mes;
+            qtdDias = resto % mes;
+
+            Console.WriteLine(qtdMeses + " mes(es)");
+            Console.WriteLine(qtdDias + " dia(s)");
+        }
+    }
+}
