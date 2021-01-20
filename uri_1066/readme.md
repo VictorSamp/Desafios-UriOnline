6a0deddfd410139104ff815a302745411b20b141
VictorSamp <victor.msampaio@outlook.com.br>
2021-01-20 19:46:16 -0300

    Finalizei o deafio uri_1066

---
+++ uri_1066/readme.md
@@ -0,0 +1,17 @@
+<center><h1>DESAFIO 1065 URI ONLINE</h1></center>
+
+Faça um programa que leia 5 valores inteiros. Conte quantos destes valores digitados são pares e mostre esta informação.
+
+<h3>Entrada:</h3>
+
+O arquivo de entrada contém 5 valores inteiros quaisquer.
+
+<h3>Saída:</h3>
+
+Imprima a mensagem conforme o exemplo fornecido, indicando a quantidade de valores pares lidos.
+
+
+
+| Exemplo de Entrada               | Exemplo de Saída |
+| -------------------------------- | :--------------: |
+| 7<br />-5<br />6<br />-4<br />12 | 3 valores pares  |
---
+++ uri_1066/uri_1066/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace uri_1066
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input, contadorPar = 0, contadorImpar = 0, contadorPositivo = 0, contadorNegativo = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                input = int.Parse(Console.ReadLine());
+
+                if (input % 2 == 0)
+                {
+                    contadorPar += 1;
+                }
+                else
+                {
+                    contadorImpar += 1;
+                }
+
+                if (input > 0)
+                {
+                    contadorPositivo += 1;
+                }
+                else if (input < 0)
+                {
+                    contadorNegativo += 1;
+                }
+            }
+
+            Console.WriteLine("{0} valor(es) par(es)", contadorPar);
+            Console.WriteLine("{0} valor(es) impar(es)", contadorImpar);
+            Console.WriteLine("{0} valor(es) positivo(s)", contadorPositivo);
+            Console.WriteLine("{0} valor(es) negativo(s)", contadorNegativo);
+
+        }
+    }
+}
