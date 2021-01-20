f71f20cceafc6835a434576022c0b32684a95ae1
VictorSamp <victor.msampaio@outlook.com.br>
2021-01-20 18:12:25 -0300

    Finalizei o desafio 'Uri 1043'

---
+++ uri_1043/readme.md
@@ -0,0 +1,24 @@
+<center><h1>DESAFIO 1042 URI ONLINE</h1></center>
+
+Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo. Em caso positivo, calcule o perímetro do triângulo e apresente a mensagem:
+
+Perimetro = XX.X
+
+Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem
+
+Area = XX.X
+
+<h3>Entrada:</h3>
+
+A entrada contém três valores reais.
+
+<h3>Saída:</h3>
+
+O resultado deve ser apresentado com uma casa decimal.
+
+
+
+| Exemplo de Entrada | Exemplo de Saída |
+| ------------------ | :--------------: |
+| 6.0 4.0 2.0        |   Area = 10.0    |
+| 6.0 4.0 2.1        | Perimetro = 12.1 |
---
+++ uri_1043/uri_1043/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri_1043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input;
+            double ladoA, ladoB, ladoC, area, perimetro;
+
+            input = Console.ReadLine().Split(' ');
+
+            ladoA = double.Parse(input[0], CultureInfo.InvariantCulture);
+            ladoB = double.Parse(input[1], CultureInfo.InvariantCulture);
+            ladoC = double.Parse(input[2], CultureInfo.InvariantCulture);
+
+            if (ladoA < (ladoB + ladoC) && ladoB < (ladoA + ladoC) && ladoC < (ladoA + ladoB))
+            {
+                perimetro = ladoA + ladoB + ladoC;
+                Console.WriteLine("Perimetro = {0}", perimetro.ToString("F1", CultureInfo.InvariantCulture));
+                return;
+            }
+
+            area = ((ladoA + ladoB) * ladoC) / 2;
+            Console.WriteLine("Area = {0}", area.ToString("F1",CultureInfo.InvariantCulture));
+        }
+    }
+}
