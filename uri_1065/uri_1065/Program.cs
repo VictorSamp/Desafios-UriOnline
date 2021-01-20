8976acf0df5c8e61b8c22659b3f23b82847db418
VictorSamp <victor.msampaio@outlook.com.br>
2021-01-20 18:56:34 -0300

    Finalizei o desafio Uri 1065

---
+++ uri_1065/uri_1065/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uri_1065
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input, contador = 0;            
+
+            for (int i = 0; i < 5; i++)
+            {
+                input = int.Parse(Console.ReadLine());
+
+                if (input % 2 == 0)
+                {
+                    contador += 1;
+                }
+            }
+
+            Console.WriteLine("{0} valores pares", contador);
+        }
+    }
+}
