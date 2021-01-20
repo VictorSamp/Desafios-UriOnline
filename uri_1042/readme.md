aa0894fd8fc6acef89e280604a05984421788f1b
VictorSamp <victor.msampaio@outlook.com.br>
2021-01-20 16:04:37 -0300

    Finalizei o desafio 'Uri 1042'

---
+++ uri_1042/readme.md
@@ -0,0 +1,20 @@
+<center><h1>DESAFIO 1042 URI ONLINE</h1></center>
+
+<p>Leia 3 valores inteiros e ordene-os em ordem crescente. No final, mostre os valores em ordem crescente, uma linha em branco e em seguida, os valores na sequência como foram lidos.</p>
+
+
+
+<h3>Entrada:</h3>
+
+<p>A entrada contem três números inteiros.</p>
+
+<h3>Saída:</h3>
+
+<p>Imprima a saída conforme foi especificado.</p>
+
+
+
+| Exemplo de Entrada |                 Exemplo de Saída                 |
+| ------------------ | :----------------------------------------------: |
+| 7 21 -14           | -14<br />7<br />21<br /><br />7<br />21<br />-14 |
+| -14 21 7           | -14<br />7<br />21<br /><br />-14<br />21<br />7 |
---
+++ uri_1042/uri_1042/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace uri_1042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input;
+            int[] inputConvertido, inputOrdenado;
+
+            input = Console.ReadLine().Split(' ');
+
+            inputConvertido = new int[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                inputConvertido[i] = int.Parse(input[i]);
+            }
+
+            inputOrdenado = new int[inputConvertido.Length];
+
+            for (int j = 0; j < inputConvertido.Length; j++)
+            {
+                inputOrdenado[j] = inputConvertido[j];
+            }
+
+            Array.Sort(inputOrdenado);
+
+            foreach (int numero in inputOrdenado)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.WriteLine("");
+
+            foreach (int numero in inputConvertido)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
