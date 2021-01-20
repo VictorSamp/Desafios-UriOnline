4f177d826d5b419b182ada5edb82cf38b841328d
VictorSamp <victor.msampaio@outlook.com.br>
2021-01-20 20:56:57 -0300

    Desafio uri 1115 finalizado e adicionando gitignore no diretorio

---
+++ uri_1115/readme.md
@@ -0,0 +1,21 @@
+<center><h1>DESAFIO 1115 URI ONLINE</h1></center>
+
+Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
+
+<h3>Entrada:</h3>
+
+A entrada contém vários casos de teste. Cada caso de teste contém 2 valores inteiros.
+
+<h3>Saída:</h3>
+
+Para cada caso de teste mostre em qual quadrante do sistema cartesiano se encontra a coordenada lida, conforme o exemplo.
+
+
+
+| Exemplo de Entrada | Exemplo de Saída |
+| ------------------ | :--------------: |
+| 2 2                |     primeiro     |
+| 3 -2               |      quarto      |
+| -8 -1              |     terceiro     |
+| -7 1               |     segundo      |
+| 0 2                |                  |
---
+++ uri_1115/uri_1115/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace uri_1115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input;
+            int x, y;
+
+            input = Console.ReadLine().Split(' ');
+
+            x = int.Parse(input[0]);
+            y = int.Parse(input[1]);
+
+            while (x != 0 || y != 0)
+            {
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("primeiro");
+                }
+                if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("segundo");
+                }
+                if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("terceiro");
+                }
+                if (x > 0 && y < 0)
+                {
+                    Console.WriteLine("quarto");
+                }
+
+                input = Console.ReadLine().Split(' ');
+                x = int.Parse(input[0]);
+                y = int.Parse(input[1]);
+            }
+            
+        }
+    }
+}
