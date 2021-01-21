21ce095fe70f00a6fbad6a658b9fa9e1f3089c73
VictorSamp <victor.msampaio@outlook.com.br>
2021-01-21 11:27:44 -0300

    Finalizei o desafio Uri 1159

---
+++ uri_1159/readme.md
@@ -0,0 +1,18 @@
+<center><h1>DESAFIO 1159 URI ONLINE</h1></center>
+
+O programa deve ler um valor inteiro **X** indefinidas vezes. (O programa irá parar quando o valor de X for igual a 0). Para cada **X** lido, imprima a soma dos 5 pares consecutivos a partir de **X**, inclusive o **X** , se for par. Se o valor de entrada for 4, por exemplo, a saída deve ser 40, que é o resultado da operação: 4+6+8+10+12, enquanto que se o valor de entrada for 11, por exemplo, a saída deve ser 80, que é a soma de 12+14+16+18+20.
+
+<h3>Entrada:</h3>
+
+O arquivo de entrada contém muitos valores inteiros. O último valor do arquivo é zero.
+
+<h3>Saída:</h3>
+
+Imprima a saida conforme a explicação acima e o exemplo abaixo.
+
+
+
+| Exemplo de Entrada | Exemplo de Saída |
+| ------------------ | :--------------- |
+| 4<br />11<br />0   | 40<br />80       |
+
---
+++ uri_1159/uri_1159/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace uri_1159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            List<int> listaNumeros = new List<int> { };
+
+            input = int.Parse(Console.ReadLine());
+
+            while (input != 0)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    if (input % 2 == 0)
+                    {
+                        listaNumeros.Add(input);
+                    }
+                    else
+                    {
+                        i--;
+                    }
+
+                    input++;
+                }
+
+                Console.WriteLine(listaNumeros.Sum());
+                listaNumeros.Clear();
+
+                input = int.Parse(Console.ReadLine());
+
+            }
+        }
+    }
+}
